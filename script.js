bf18a18a26b5d18260d638019f59f02f8ba0ed9a
Am1d1v <dimadiman1adima@gmail.com>
2024-07-26 14:52:46 +0300

    Display word function. Play again button

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 
 
+const wordEl = document.querySelector('#word');
+const wrongLetterEl = document.querySelector('#wrong-letter');
+const playAgainButton = document.querySelector('#play-button');
+const popup = document.querySelector('#container-popup');
+const notification = document.querySelector('#container-notification');
+const finalMessage = document.querySelector('#final-message');
+const figureParts = document.querySelectorAll('.figure-part');
+
+// Set of Words
+const words = ['black', 'white', 'pink'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ['p', 'i', 'n', 'k'];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord(){
+    wordEl.innerHTML = `
+    ${selectedWord.split('').map(letter => `<span class="letter">${correctLetters.includes(letter) ? letter : ''}</span>`).join('')}
+    `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
+    
+    if(innerWord === selectedWord){
+        finalMessage.innerText = 'Congratulations';
+        popup.style.display = 'flex';
+    }
+};  
+displayWord();
+
+// Reload page
+playAgainButton.addEventListener('click', () => window.location.reload());
