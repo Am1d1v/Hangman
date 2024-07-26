eb697db94cc730203aa6e7d582b7f49d91ad4816
Am1d1v <dimadiman1adima@gmail.com>
2024-07-26 15:37:34 +0300

    Letter Press Event. Show Notification

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const words = ['black', 'white', 'pink'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['p', 'i', 'n', 'k'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
@@ -30,6 +30,53 @@ function displayWord(){
     }
 };  
 displayWord();
+
+// Update the wrong letters
+function updateWrongLettersEl(){
+    console.log('Update wrong');
+}
+
+// Show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 1000);
+
+}
+
+// Keydown letter press
+window.addEventListener('keydown', (e) => {
+    // Only letters allowed
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+        
+        if(selectedWord.includes(letter)){
+
+            if(!correctLetters.includes(letter)){
+
+                correctLetters.push(letter);
+                displayWord();
+
+            } else {
+                showNotification();
+            }
+            
+        } else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+
+    }
+
+
+}) ;
+
 
 // Reload page
 playAgainButton.addEventListener('click', () => window.location.reload());
